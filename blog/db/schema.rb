8a9fc36a7648a4150431fc7cf35faf242ffcbba5
Kevin <stradtkt22@gmail.com>
2019-02-19 08:00:49 -0500

    Added Pages model

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_121359) do
+ActiveRecord::Schema.define(version: 2019_02_19_125920) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
